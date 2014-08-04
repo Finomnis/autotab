c3284917ea9eb28b5ca1a4b9526399b69a6c5497
Finomnis <martin.h.stumpf@gmail.com>
2014-08-04 17:44:40 -0500

    more beautification.

--- main.cpp
+++ main.cpp
@@ -12,21 +12,30 @@ namespace autotab{
     
         // read first line
         std::getline(input, newline);
+
+        // initialize table with first line
         table t(newline);
     
         while(input.good())
         {
+            // read next line
             std::getline(input, newline);
     
+            // add to table
             if(!t.add_row(newline))
             {
-                output << t;
+                // if line isn't compatible with table, flush table
+                output << t << std::endl;
+
+                // create new table
                 t = table(newline);
-                output << std::endl;
             }        
     
         }
     
+        // flush final table. no newline.
+        // (the last line of a file is by definition not followed by a newline,
+        //  otherwise it wouldn't be the last line of the file.)
         output << t;
     }
 
@@ -35,29 +44,52 @@ namespace autotab{
 
 int main(int argc, char **argv)
 {
-    if(argc == 1)
+    // print help message if help is requested or more than one
+    // argument is provided
+    if(argc > 1)
     {
-        autotab::autotab(std::cin, std::cout);
-    }
-
-    if(argc == 2)
-    {
-        if(std::string(argv[1]) == std::string("-h"))
+        if(std::string(argv[1]) == std::string("-h") || argc > 2)
         {
             std::cout << "Usage: " << argv[0] << " [input]" << std::endl;
             return 2;
         }
+    }
 
+    // if no file is provided, read from std::cin
+    if(argc == 1)
+    {
+        // process
+        autotab::autotab(std::cin, std::cout);
+
+        // success!
+        return 0;
+    }
+
+    // if file is provided, read from file
+    if(argc == 2)
+    {
+       
+        // open file
         std::ifstream fin(argv[1]);
+
+        // check if file was successfully opened
         if(!fin.good())
         {
             std::cerr << "Error: Unable to open file '" << argv[1] << "'!"
                      << std::endl;
             return 1;
         }
+
+        // process
         autotab::autotab(fin, std::cout); 
+
+        // close file
         fin.close();
+
+        // success!
+        return 0;
     }
    
-    return 0;
+    // should never happen
+    return 1;
 }
